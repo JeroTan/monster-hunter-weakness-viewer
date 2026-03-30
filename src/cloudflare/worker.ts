1611d1d788c2d3e7914d12f0b6e383496035a0da
Tan Jerowe <86645548+JeroTan@users.noreply.github.com>
2026-03-30 18:05:43 +0800

    chore: fix to align astro 16

--- src/cloudflare/worker.ts
+++ src/cloudflare/worker.ts
@@ -1,21 +1,12 @@
-import type { SSRManifest } from "astro";
 import { handle } from '@astrojs/cloudflare/handler';
-import { ExampleDurableObject } from "./durable-objects/ExampleDurableObject";
+export * from "./durable-objects/ExampleDurableObject";
 
-export function createExports(manifest: SSRManifest) {
-    return {
-        default: {
-            async fetch(request, env, ctx) {
-                return handle(request, env, ctx);
-            },
-            async queue(batch, _env) {
-                let messages = JSON.stringify(batch.messages);
-                console.log(`consumed from our queue: ${messages}`);
-            },
-            async scheduled(event, env, ctx){
-                // Do some time logic heere
-            },
-        } satisfies ExportedHandler<Env>,
-        ExampleDurableObject:ExampleDurableObject
-    };
-}
+export default {
+  async fetch(request, env, ctx) {
+    return handle(request, env, ctx);
+  },
+  async queue(batch, _env) {
+    let messages = JSON.stringify(batch.messages);
+    console.log(`consumed from our queue: ${messages}`);
+  },
+} satisfies ExportedHandler<Env>;
